57831b0c2052235b5cf492a59c4cdd557ee7cb73
elasticspoon <mail@yuribocharov.dev>
2025-07-04 15:41:40 -0400

    feat: use latest ruby

--- src/engine/wasi/boxes.js
+++ src/engine/wasi/boxes.js
@@ -1,33 +1,33 @@
 // WASI binaries.
 export default {
-    lua: {
-        program: "lua",
-        repo: "https://github.com/nalgeon/lua-wasi",
-        path: "https://unpkg.com/@antonz/lua-wasi@5.4.6/dist",
-        file: "lua.wasm",
-    },
-    php: {
-        program: "php",
-        repo: "https://github.com/nalgeon/php-wasi",
-        path: "https://unpkg.com/@antonz/php-wasi@8.2.6/dist",
-        file: "php-cgi.wasm",
-    },
-    python: {
-        program: "python",
-        repo: "https://github.com/nalgeon/python-wasi",
-        path: "https://unpkg.com/@antonz/python-wasi@3.12.0/dist",
-        file: "python.wasm",
-    },
-    ruby: {
-        program: "ruby",
-        repo: "https://github.com/nalgeon/ruby-wasi",
-        path: "https://unpkg.com/@antonz/ruby-wasi@3.2.2/dist",
-        file: "ruby.wasm",
-    },
-    sqlite: {
-        program: "sqlite3",
-        repo: "https://github.com/nalgeon/sqlite-wasi",
-        path: "https://unpkg.com/@antonz/sqlite-wasi@3.44.2/dist",
-        file: "sqlite.wasm",
-    },
+  lua: {
+    program: "lua",
+    repo: "https://github.com/nalgeon/lua-wasi",
+    path: "https://unpkg.com/@antonz/lua-wasi@5.4.6/dist",
+    file: "lua.wasm",
+  },
+  php: {
+    program: "php",
+    repo: "https://github.com/nalgeon/php-wasi",
+    path: "https://unpkg.com/@antonz/php-wasi@8.2.6/dist",
+    file: "php-cgi.wasm",
+  },
+  python: {
+    program: "python",
+    repo: "https://github.com/nalgeon/python-wasi",
+    path: "https://unpkg.com/@antonz/python-wasi@3.12.0/dist",
+    file: "python.wasm",
+  },
+  ruby: {
+    program: "ruby",
+    repo: "https://github.com/ruby/ruby.wasm",
+    path: "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@latest/dist",
+    file: "ruby.wasm",
+  },
+  sqlite: {
+    program: "sqlite3",
+    repo: "https://github.com/nalgeon/sqlite-wasi",
+    path: "https://unpkg.com/@antonz/sqlite-wasi@3.44.2/dist",
+    file: "sqlite.wasm",
+  },
 };
